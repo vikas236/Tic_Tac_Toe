bc8a32f2970da13f47b032ff738d8699a9f5b7d3
sai vikas <saivikas461@gmail.com>
2023-06-04 21:27:47 +0530

    add click play button to play function and score

--- script.js
+++ script.js
@@ -1,37 +1,50 @@
 // Calling html elements
 const game = document.querySelector(".game");
-const start = document.querySelector(".play");
+const start = document.querySelector(".start");
+const box = document.querySelector(".box");
 
 
 // Function declaration
 const play = (() => {
+    window.addEventListener("keydown", function (e) {
+        if (e.key == "Escape") {
+            play.clear();
+        }
+        if (e.key == "Enter") {
+            play.show();
+        }
+    });
     const show = () => {
-        game.classList.remove("play");
-        fillBox();
+        game.classList.add("play");
+        start.style.display = "none";
+        fillStripes();
+        const shell = document.querySelectorAll(".shell");
+        fillshells(shell);
     };
-    const fillBox = () => {
-        game.innerHTML = "";
-        for (i = 0; i < 9; i++) {
-            game.appendChild(document.createElement("div"));
-            game.childNodes[i].classList.add("box");
+    const fillStripes = () => {
+        for (let i = 0; i < 9; i++) {
+            box.appendChild(document.createElement("div"));
+            box.childNodes[i].classList.add("shell");
         };
     }
+    const fillshells = (shell) => {
+        for (let i = 0; i < shell.length; i++) {
+            shell[i].addEventListener("click", function () {
+                this.innerHTML = "O";
+                console.log(this.style);
+            });
+        };
+    };
     const clear = () => {
-        game.classList.add("play");
-        game.innerHTML = "<h1>PLAY</h1>";
+        game.classList.remove("play");
+        start.style.display = "block";
     };
 
     return { show, clear };
 })();
 
 
+
 // Function calling
 start.addEventListener("click", play.show);
-window.addEventListener("keydown", function (e) {
-    if (e.key == "Escape") {
-        play.clear();
-    }
-    if (e.key == "Enter") {
-        play.show();
-    }
-});
+play.show();
