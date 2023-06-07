f7d6c02c6d58c05520e3dd5e1332948a1aa12c2f
sai vikas <saivikas461@gmail.com>
2023-06-07 21:32:11 +0530

    add bot and a difficulty selector (no functioning)

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const box = document.querySelector(".box");
 const reset = document.querySelector(".reset");
 const p1 = document.querySelector(".p1");
 const p2 = document.querySelector(".p2");
-const score = document.querySelectorAll(".points")
+const scoreBoard = document.querySelectorAll(".points")
 const decision = document.querySelector(".decision");
 const wait = document.querySelector(".wait");
 const wall = document.querySelector(".wall");
@@ -27,7 +27,7 @@ const play = (() => {
                 play.show();
             }
         });
-    }
+    };
 
     // Show tic tac toe grid
     const show = () => {
@@ -52,14 +52,14 @@ const play = (() => {
         for (let i = 0; i < shell.length; i++) {
             shell[i].addEventListener("click", function () {
                 empty = noSpace(shell);
-                if (this.innerHTML == "" && !winner(checkWinner(shell))) {
-                    winner(checkWinner(shell));
+                winner(checkWinner(shell));
+                if (this.innerHTML == "") {
                     this.style.color = "#545454";
                     this.innerHTML = "X";
                     turn = 1;
                     playerTurn(1);
                     wait.style.display = "block";
-                    setTimeout(() => { botMove(shell); }, 1000);
+                    if (empty) { setTimeout(() => { botMove(shell); }, 300); };
                 };
             });
         };
@@ -67,20 +67,18 @@ const play = (() => {
 
     // Bots turn
     const botMove = (shell) => {
-        wait.style.display = "none";
+        setTimeout(() => { wait.style.display = "none"; }, 500);
         empty = noSpace(shell);
         winner(checkWinner(shell));
-        for (let i = 0; i < shell.length; i++) {
-            let n = Math.floor(Math.random() * 8);
-            if (p) { break; };
-            if (shell[n].innerHTML == "") {
-                shell[n].style.color = "#F2EBD3";
-                shell[n].innerHTML = "O";
-                if (checkWinner(shell) == false && turn == 0) { playerTurn(0, shell); };
-                turn = 0;
-                return shell;
-            };
-        };
+        const bestMove = findBestMove(shell); // Get the best move using the minimax algorithm
+        if (bestMove.move != -1) {
+            shell[bestMove.move].style.color = "#F2EBD3";
+            shell[bestMove.move].innerHTML = "O";
+            if (checkWinner(shell) == false && turn == 0) { playerTurn(0); };
+            turn = 0;
+            empty = noSpace(shell);
+            winner(checkWinner(shell));
+        }
     };
 
     // Change player turn
@@ -100,70 +98,124 @@ const play = (() => {
         p2: 0,
     }
 
-    // Winner announcing, Starting a new game
-    const winner = (str) => {
-        let z = 0;
-        if (str == "X") { z = 1; }
-        else if (str == "O") { z = 2; };
-        if (str != false && (empty == 0)) {
-            if (points.p1 < 3 && points.p2 < 3) {
-                p = `Player ${z} has won a Point!`;
-                decision.innerHTML = p;
-                points[Object.keys(points)[z - 1]] = ++Object.values(points)[z - 1];
-                score[z - 1].innerHTML = Object.values(points)[z - 1];
-                wall.style.display = "block";
-            };
-            if (points.p1 >= 3) {
-                p = "Player 1 is the Winner!";
-                decision.innerHTML = p;
-                wall.style.display = "block";
+    // Minimax algorithm
+    const minimax = (shell, depth, maximizingPlayer) => {
+        const result = checkWinner(shell);
+        if (result === 'O') {
+            return 1; // Bot wins
+        } else if (result === 'X') {
+            return -1; // Player wins
+        } else if (result === 'draw' || depth >= 9) {
+            return 0; // It's a draw or the maximum depth is reached
+        };
+
+        if (maximizingPlayer) {
+            let bestEval = -Infinity;
+            for (let i = 0; i < shell.length; i++) {
+                if (shell[i].innerHTML === '') {
+                    shell[i].innerHTML = 'O';
+                    const eval = minimax(shell, depth + 1, false);
+                    shell[i].innerHTML = '';
+                    bestEval = Math.max(bestEval, eval);
+                }
             }
-            else if (points.p2 >= 3) {
-                p = "Player 2 is the Winner!";
-                decision.innerHTML = p;
-                wall.style.display = "block";
-            };
+            return bestEval;
+        } else {
+            let bestEval = Infinity;
+            for (let i = 0; i < 9; i++) {
+                if (shell[i].innerHTML === '') {
+                    shell[i].innerHTML = 'X';
+                    const eval = minimax(shell, depth + 1, true);
+                    shell[i].innerHTML = '';
+                    bestEval = Math.min(bestEval, eval);
+                }
+            }
+            return bestEval;
         }
-        else if (empty == 1) {
-            p = "Draw!"
-            decision.innerHTML = p;
-            wall.style.display = "block";
-            empty = 0;
-        }
-        else {
-            console.log("here");
-        };
     };
 
-    // Confirm winner
+    // Find the best move using the minimax algorithm
+    const findBestMove = (shell) => {
+        let bestScore = -Infinity;
+        let bestMove = -1;
+
+        for (let i = 0; i < 9; i++) {
+            if (shell[i].innerHTML === '') {
+                shell[i].innerHTML = 'O';
+                const score = minimax(shell, 0, false);
+                shell[i].innerHTML = '';
+
+                if (score >= bestScore) {
+                    bestScore = score;
+                    bestMove = i;
+                }
+            }
+        }
+        return { move: bestMove, score: bestScore };
+    }
+
+    // Check for the winner
     const checkWinner = (shell) => {
-        let arr = [];
-        for (i = 0; i < shell.length; i++) {
-            arr[i] = shell[i].innerHTML;
-        };
-        if (checkEqual(arr, 0, 1, 2)) { return arr[0]; }
-        else if (checkEqual(arr, 2, 5, 8)) { return arr[2]; }
-        else if (checkEqual(arr, 0, 3, 6)) { return arr[0]; }
-        else if (checkEqual(arr, 6, 7, 8)) { return arr[6]; }
-        else if (checkEqual(arr, 1, 4, 7)) { return arr[1]; }
-        else if (checkEqual(arr, 3, 4, 5)) { return arr[3]; }
-        else if (checkEqual(arr, 0, 4, 8)) { return arr[0]; }
-        else if (checkEqual(arr, 2, 4, 6)) { return arr[2]; }
-        else { return false; };
-    };
+        const winningCombinations = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],
+            [0, 4, 8], [2, 4, 6]
+        ];
 
-    // Check if 3 values consecutively are equal for a win
-    const checkEqual = (arr, a, b, c) => {
-        if (arr[a] == arr[b] && arr[b] == arr[c] && arr[a] != "") {
-            return true;
+        for (let i = 0; i < winningCombinations.length; i++) {
+            const [a, b, c] = winningCombinations[i];
+            if (shell[a].innerHTML === shell[b].innerHTML && shell[a].innerHTML == shell[c].innerHTML && shell[a].innerHTML !== "") {
+                return shell[a].innerHTML;
+            }
         }
-        else { return false; };
+
+        let draw = true;
+        for (let i = 0; i < 9; i++) {
+            if (shell[i].innerHTML === "") {
+                draw = false;
+                break;
+            }
+        }
+        if (draw) {
+            return "draw";
+        }
+
+        return false;
+    }
+
+    // Count empty spaces in the grid
+    const noSpace = (shell) => {
+        let empty = 0;
+        for (let i = 0; i < 9; i++) {
+            if (shell[i].innerHTML === "") {
+                empty++;
+            }
+        }
+        return empty;
+    }
+
+    // Determine the winner and update score
+    const winner = (result) => {
+        if (result === "O") {
+            points.p2++;
+            scoreBoard[1].innerHTML = points.p2;
+            decision.innerHTML = "Bot Wins!";
+            wall.style.display = "block";
+        } else if (result === "X") {
+            points.p1++;
+            scoreBoard[0].innerHTML = points.p1;
+            decision.innerHTML = "Player Wins!";
+            wall.style.display = "block";
+        } else if (result === "draw") {
+            decision.innerHTML = "Draw!";
+            wall.style.display = "block";
+        }
     };
 
     // Wall to stop player interaction after gaining a point or game over
     const dim = () => {
         wall.addEventListener("click", function () {
-            if (points.p1 >= 3 || points.p1 >= 3) {
+            if (points.p1 >= 3 || points.p2 >= 3) {
                 reset();
                 close();
                 decision.innerHTML = "";
@@ -172,29 +224,19 @@ const play = (() => {
             else if (points.p1 < 3 && points.p2 < 3) {
                 clear();
             }
-            else if (empty == 1) {
-                decision.innerHTML = "Draw";
+            else if (empty == 0) {
+                decision.innerHTML = "";
                 clear();
                 empty = noSpace([]);
             };
         });
     };
 
-    // Check if the grid is full
-    const noSpace = (arr) => {
-        for (let i = 0; i < arr.length; i++) {
-            if (arr[i].innerHTML == "") {
-                return 0;
-            }
-        };
-        return 1;
-    };
-
     // Reset the game
     const reset = () => {
         clear();
-        score[0].innerHTML = 0;
-        score[0].innerHTML = 0;
+        scoreBoard[0].innerHTML = 0;
+        scoreBoard[1].innerHTML = 0;
         points.p1 = 0;
         points.p2 = 0;
         empty = 0;
@@ -216,11 +258,10 @@ const play = (() => {
         turn = 0;
         playerTurn(0);
         p = "";
-        empty = 0;
         decision.innerHTML = "";
     };
 
-    return { keys, show, dim, clear, reset, close, botMove };
+    return { keys, show, reset, close, dim };
 })();
 
 
@@ -229,5 +270,4 @@ const play = (() => {
 start.addEventListener("click", play.show);
 reset.addEventListener("click", function () { play.reset(); play.close(); });
 play.keys();
-play.show();
 play.dim();
